48b74934d24c02899ad46fb837356f80fec2bfcf
Daniyar <dan_ntu@mail.ru>
2018-10-28 20:49:02 +0600

    Initial commit

---
+++ src/main/java/kz/logistic/pl/PlApplication.java
@@ -0,0 +1,12 @@
+package kz.logistic.pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlApplication.class, args);
+  }
+}
