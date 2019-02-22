b9b1126ac5bf4219c3c7f922a92ce3d619625247
Dastan <dan_ntu@mail.ru>
2019-02-22 15:05:18 +0600

    Implemented delete and update functionality to productSubCategory

--- src/main/java/kz/logistic/pl/services/impl/DefaultProductSubCategoryService.java
+++ src/main/java/kz/logistic/pl/services/impl/DefaultProductSubCategoryService.java
@@ -2,12 +2,13 @@ package kz.logistic.pl.services.impl;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import kz.logistic.pl.models.entities.ProductsSubCategoryEntity;
 import kz.logistic.pl.models.factories.LocalizedMessageBuilderFactory;
 import kz.logistic.pl.models.pojos.ProductSubCategory;
-import kz.logistic.pl.models.pojos.impl.DefaulProductSubCategory;
+import kz.logistic.pl.models.pojos.impl.DefaultProductSubCategory;
 import kz.logistic.pl.models.pojos.json.ProductSubCategoryJson;
 import kz.logistic.pl.repositories.ProductSubCategoryRepository;
 import kz.logistic.pl.services.ProductSubCategoryService;
@@ -37,7 +38,7 @@ public class DefaultProductSubCategoryService implements ProductSubCategoryServi
   public List<ProductSubCategory> showAllProductSubCategory() {
     List<ProductsSubCategoryEntity> entities = this.productSubCategoryRepository.findAll();
 
-    return entities.stream().map(productsSubCategoryEntity -> DefaulProductSubCategory.builder()
+    return entities.stream().map(productsSubCategoryEntity -> DefaultProductSubCategory.builder()
       .id(productsSubCategoryEntity.getProductSubcategoryId())
       .subCategoryName(
         localizedMessageBuilderFactory.builder()
@@ -51,18 +52,18 @@ public class DefaultProductSubCategoryService implements ProductSubCategoryServi
 
 
   @Override
-  public void addProductSubCategory(String subCategoryNameKk, String subCategoryNameRu,
-                                    String subCategoryNameEn, Long productCategoryId,
-                                    String subCategoryAddInfo) {
+  public void addProductSubCategory(String subSubCategoryNameKk, String subSubCategoryNameRu,
+                                    String subSubCategoryNameEn, Long productCategoryId,
+                                    String subSubCategoryAddInfo) {
     ProductsSubCategoryEntity subCategoryEntity = new ProductsSubCategoryEntity();
-    subCategoryEntity.setSubCategoryNameKk(subCategoryNameKk);
-    subCategoryEntity.setSubCategoryNameRu(subCategoryNameRu);
-    subCategoryEntity.setSubCategoryNameEn(subCategoryNameEn);
+    subCategoryEntity.setSubCategoryNameKk(subSubCategoryNameKk);
+    subCategoryEntity.setSubCategoryNameRu(subSubCategoryNameRu);
+    subCategoryEntity.setSubCategoryNameEn(subSubCategoryNameEn);
     subCategoryEntity.setProductCategoryId(productCategoryId);
-    subCategoryEntity.setSubCategoryAddInfo(subCategoryAddInfo);
+    subCategoryEntity.setSubCategoryAddInfo(subSubCategoryAddInfo);
 
     this.productSubCategoryRepository.save(subCategoryEntity);
-    log.info("Added new ProductSubCategory: " + subCategoryNameRu + " " + new Date());
+    log.info("Added new ProductSubCategory: " + subSubCategoryNameRu + " " + new Date());
   }
 
   @Override
@@ -78,4 +79,64 @@ public class DefaultProductSubCategoryService implements ProductSubCategoryServi
     log.info("Added new ProductSubCategory: "
       + productSubCategoryJson.getSubCategoryNameRu() + " via Json " + new Date());
   }
+
+  @Override
+  public String updateProductSubCategory(
+    Long productSubCategoryId, ProductSubCategoryJson productSubCategoryJson) {
+    ProductsSubCategoryEntity productsSubCategoryEntity =
+      this.productSubCategoryRepository.findById(productSubCategoryId).orElse(null);
+
+    if (Objects.nonNull(productsSubCategoryEntity)) {
+      if (productSubCategoryJson.getSubCategoryNameKk() != null) {
+        productsSubCategoryEntity.setSubCategoryNameKk(productSubCategoryJson.getSubCategoryNameKk());
+      }
+      if (productSubCategoryJson.getSubCategoryNameRu() != null) {
+        productsSubCategoryEntity.setSubCategoryNameRu(productSubCategoryJson.getSubCategoryNameRu());
+      }
+      if (productSubCategoryJson.getSubCategoryNameEn() != null) {
+        productsSubCategoryEntity.setSubCategoryNameEn(productSubCategoryJson.getSubCategoryNameEn());
+      }
+      if (productSubCategoryJson.getSubCategoryAddInfo() != null) {
+        productsSubCategoryEntity.setSubCategoryAddInfo(productSubCategoryJson.getSubCategoryAddInfo());
+      }
+      if (productSubCategoryJson.getProductCategoryId() != null){
+        productsSubCategoryEntity.setProductCategoryId(productSubCategoryJson.getProductCategoryId());
+      }
+      this.productSubCategoryRepository.save(productsSubCategoryEntity);
+      log.info("Updated  product category " + new Date());
+      return "Подкатегория продуктов обновлена";
+    } else {
+      return "Подкатегория продуктов с таким id не существует";
+    }
+  }
+
+  @Override
+  public String deleteProductSubCategory(Long productCategoryId) {
+    ProductsSubCategoryEntity productsSubCategoryEntity =
+      this.productSubCategoryRepository.findById(productCategoryId).orElse(null);
+
+    if (Objects.nonNull(productsSubCategoryEntity)) {
+      log.info("Deleted " + productsSubCategoryEntity.getSubCategoryNameRu() + " category " + new Date());
+      this.productSubCategoryRepository.delete(productsSubCategoryEntity);
+      return "Подкатегория продукта удалена";
+    } else {
+      return "Подкатегория продукта с таким id не существует";
+    }
+  }
+
+    @Override
+    public ProductSubCategory showProductSubCategory(Long productSubCategoryId) {
+      ProductsSubCategoryEntity productsSubCategoryEntity =
+        this.productSubCategoryRepository.findById(productSubCategoryId).orElse(null);
+      return DefaultProductSubCategory.builder()
+        .id(productSubCategoryId)
+        .subCategoryName(localizedMessageBuilderFactory.builder()
+          .en(productsSubCategoryEntity.getSubCategoryNameEn())
+          .kk(productsSubCategoryEntity.getSubCategoryNameKk())
+          .ru(productsSubCategoryEntity.getSubCategoryNameRu()).build())
+        .productCategoryId(productsSubCategoryEntity.getProductCategoryId())
+        .subCategoryAddInfo(productsSubCategoryEntity.getSubCategoryAddInfo()).build();
+    }
+
+
 }
