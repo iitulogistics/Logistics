dde0c606be265f0ae98a6847bb1f4aa7851ae385
МУИТ <dan_ntu@mail.ru>
2019-03-06 20:31:23 +0300

    Для CORS добавил все методы

--- src/main/java/kz/logistic/pl/configs/WebConfig.java
+++ src/main/java/kz/logistic/pl/configs/WebConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("*");
+    registry.addMapping("/**").allowedOrigins("*")
+      .allowedMethods(String.valueOf(Arrays.asList("GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS")));
   }
 
 }
