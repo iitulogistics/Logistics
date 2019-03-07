8d1f89ee5fce6e75e678b1f6a6b769bda8ce7bda
danik <dan_ntu@mail.ru>
2019-03-07 17:49:39 +0600

    Временная таблица для OTP

---
+++ src/main/java/kz/logistic/pl/repositories/OtpRepository.java
@@ -0,0 +1,13 @@
+package kz.logistic.pl.repositories;
+
+import kz.logistic.pl.models.entities.OtpEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface OtpRepository extends JpaRepository<OtpEntity, Long> {
+  List<OtpEntity> findByMobilePhoneAndOtp(String mobileNumber, String otp);
+}
