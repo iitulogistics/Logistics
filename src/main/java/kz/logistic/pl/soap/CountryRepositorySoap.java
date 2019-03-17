cb0b2680c3a96e7f173e2571ceb2665c92bdefa2
Myrzakanov Daniyar <dan_ntu@mail.ru>
2019-03-17 18:09:54 +0600

    SOAP country show by id

---
+++ src/main/java/kz/logistic/pl/soap/CountryRepositorySoap.java
@@ -0,0 +1,44 @@
+package kz.logistic.pl.soap;
+
+import kz.logistic.pl.models.entities.CountryEntity;
+import kz.logistic.pl.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import soap.logistic.logistics.Country;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class CountryRepositorySoap {
+    private static final Map<Long, Country> countryMap = new HashMap<>();
+
+    private CountryRepository repository;
+
+    @Autowired(required = false)
+    public void setRepository(CountryRepository repository) {
+      this.repository = repository;
+    }
+
+    @PostConstruct
+    public void initData() {
+       List<CountryEntity> entities = this.repository.findAll();
+       entities.forEach(countryEntity -> {
+         Country country = new Country();
+
+         country.setId(countryEntity.getCountryId());
+         country.setCountryNameKk(countryEntity.getCountryNameKk());
+         country.setCountryNameRu(countryEntity.getCountryNameRu());
+         country.setCountryNameEn(countryEntity.getCountryNameEn());
+
+         countryMap.put(country.getId(), country);
+
+       });
+    }
+
+    public Country findCountryId(Long id) {
+        return countryMap.get(id);
+    }
+}
