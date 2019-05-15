a9396520004cf1a8e228e6afa4a52a57eca9ff8b
Karim <karim20.ks@gmail.com>
2019-05-15 15:24:50 +0600

    Убраны лишние поля и добавлен переход по страницам

--- src/main/java/kz/logistic/pl/poi/ReadExcelFile.java
+++ src/main/java/kz/logistic/pl/poi/ReadExcelFile.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class ReadExcelFile {
 
-  public List<ProductsEntity> readProductFromExcelFile(MultipartFile multipartFile) throws IOException {
+  public List<ProductsEntity> readProductFromExcelFile(MultipartFile multipartFile, Long seller_company_id) throws IOException {
 
     FileInputStream fileInputStream = (FileInputStream) multipartFile.getInputStream();
 
@@ -29,48 +29,46 @@ public class ReadExcelFile {
       workbook = new HSSFWorkbook(fileInputStream);
 
     }
-    Sheet sheet = workbook.getSheet("Sheet1");
 
     List<ProductsEntity> list = new ArrayList<>();
 
-    for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
+    for (int sheetIndex = 0; sheetIndex < workbook.getNumberOfSheets(); sheetIndex++) {
+      Sheet sheet = workbook.getSheetAt(sheetIndex);
+      for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
 
-      Row row = sheet.getRow(i);
+        Row row = sheet.getRow(i);
 
-      String product_name_kk = row.getCell(0).getStringCellValue(),
-        product_name_ru = row.getCell(1).getStringCellValue(),
-        product_name_en = row.getCell(2).getStringCellValue(),
-        unique_id_number = row.getCell(4).getStringCellValue(),
-        serial_namber = row.getCell(5).getStringCellValue(),
-        manufacturer = row.getCell(6).getStringCellValue(),
-        size = row.getCell(7).getStringCellValue(),
-        product_description = row.getCell(10).getStringCellValue(),
-        product_img = row.getCell(14).getStringCellValue();
-      long product_subcategory_id = (long) row.getCell(3).getNumericCellValue(),
-        seller_company_id = (int) row.getCell(11).getNumericCellValue(),
-        special_characteristic_id = (int) row.getCell(12).getNumericCellValue(),
-        product_category_id = (int) row.getCell(13).getNumericCellValue();
-      int weight = (int) row.getCell(8).getNumericCellValue(),
-        price = (int) row.getCell(9).getNumericCellValue();
+        String product_name_kk = row.getCell(0).getStringCellValue(),
+          product_name_ru = row.getCell(1).getStringCellValue(),
+          product_name_en = row.getCell(2).getStringCellValue(),
+          unique_id_number = row.getCell(3).getStringCellValue(),
+          serial_namber = row.getCell(4).getStringCellValue(),
+          manufacturer = row.getCell(5).getStringCellValue(),
+          size = row.getCell(6).getStringCellValue(),
+          product_description = row.getCell(9).getStringCellValue();
 
-      ProductsEntity productsEntity = new ProductsEntity();
-      productsEntity.setProductNameEn(product_name_en);
-      productsEntity.setProductNameRu(product_name_ru);
-      productsEntity.setProductNameKk(product_name_kk);
-      productsEntity.setUniqueIdNumber(unique_id_number);
-      productsEntity.setSerialNumber(serial_namber);
-      productsEntity.setSize(size);
-      productsEntity.setProductDescription(product_description);
-      productsEntity.setProductsImg(product_img);
-      productsEntity.setManufacturer(manufacturer);
-      productsEntity.setProductSubcategoryId(product_subcategory_id);
-      productsEntity.setWeight(weight);
-      productsEntity.setPrice(price);
-      productsEntity.setSellerCompanyId(seller_company_id);
-      productsEntity.setSpecialCharacteristicId(special_characteristic_id);
-      productsEntity.setProductCategoryId(product_category_id);
+        int weight = (int) row.getCell(7).getNumericCellValue(),
+          price = (int) row.getCell(8).getNumericCellValue();
 
-      list.add(productsEntity);
+        ProductsEntity productsEntity = new ProductsEntity();
+        productsEntity.setProductNameEn(product_name_en);
+        productsEntity.setProductNameRu(product_name_ru);
+        productsEntity.setProductNameKk(product_name_kk);
+        productsEntity.setUniqueIdNumber(unique_id_number);
+        productsEntity.setSerialNumber(serial_namber);
+        productsEntity.setSize(size);
+        productsEntity.setProductDescription(product_description);
+//        productsEntity.setProductsImg(product_img);
+        productsEntity.setManufacturer(manufacturer);
+//        productsEntity.setProductSubcategoryId(product_subcategory_id);
+        productsEntity.setWeight(weight);
+        productsEntity.setPrice(price);
+        productsEntity.setSellerCompanyId(seller_company_id);
+//        productsEntity.setSpecialCharacteristicId(special_characteristic_id);
+//        productsEntity.setProductCategoryId(product_category_id);
+
+        list.add(productsEntity);
+      }
     }
 
     return list;
