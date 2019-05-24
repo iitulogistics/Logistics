1e48c00655086b6eecf372daf30829d33c5d150d
Данияр <dan_ntu@mail.ru>
2019-05-24 13:28:44 +0600

    Удалил лишние пробелы

--- src/main/java/kz/logistic/pl/repositories/RegionRepository.java
+++ src/main/java/kz/logistic/pl/repositories/RegionRepository.java
@@ -1,14 +1,12 @@
 package kz.logistic.pl.repositories;
 
-import java.util.ArrayList;
-
 import kz.logistic.pl.models.entities.RegionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.JoinColumn;
+import java.util.ArrayList;
 
 
 public interface RegionRepository extends JpaRepository<RegionEntity, Long> {
