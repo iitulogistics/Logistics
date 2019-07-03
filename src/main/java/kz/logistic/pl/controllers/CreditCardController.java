7f75c395dd9f225f17aa3c132128076cdcf6981d
Myrzakanov Daniyar <dan_ntu@mail.ru>
2019-07-03 19:01:43 +0600

    Кредитная карта удалена

--- src/main/java/kz/logistic/pl/controllers/CreditCardController.java
+++ src/main/java/kz/logistic/pl/controllers/CreditCardController.java
@@ -12,54 +12,54 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.Timestamp;
 import java.util.Date;
 
-@Api(tags = {"Список кредитных карт клиентов не использовать"}, description = "API для списка кредитных карт")
+//@Api(tags = {"Список кредитных карт клиентов не использовать"}, description = "API для списка кредитных карт")
 @RestController
 @RequestMapping(value = "/credit/card")
 public class CreditCardController {
-    private CreditCardService creditCardService;
-
-    @Qualifier("defaultCreditCardService")
-    @Autowired(required = false)
-    public void setCreditCardService(CreditCardService creditCardService) {
-        this.creditCardService = creditCardService;
-    }
-
-    @ApiOperation(value = "Показвает весь список кредитных карт")
-    @GetMapping("/all")
-    public ResponseEntity<?> all() {
-        return ResponseEntity.ok(this.creditCardService.showAllCreditCard());
-    }
-
-    @ApiOperation(value = "Показвает кредитную карту по ID")
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getId(@PathVariable(value = "id") Long creditCardId) {
-        return ResponseEntity.ok(this.creditCardService.showCreditCard(creditCardId));
-    }
-
-    @ApiOperation(value = "Добавляет кредитную карту")
-    @PostMapping("/add")
-    public ResponseEntity<?> add(
-        @RequestParam Integer creditCardNumber,
-        @RequestParam String holderName,
-        @RequestParam Date expireDate) {
-        return ResponseEntity.ok(this.creditCardService.addCreditCard(creditCardNumber, holderName, expireDate));
-    }
-
-    @ApiOperation(value = "Добавляет кредитную карту посредством JSON")
-    @PostMapping("/addJson")
-    public ResponseEntity<?> addJson(@RequestBody CreditCardJson creditCardJson) {
-        return ResponseEntity.ok(this.creditCardService.addCreditCardJson(creditCardJson));
-    }
-
-    @ApiOperation(value = "Обновляет кредитную карту")
-    @PatchMapping("{id}")
-    public ResponseEntity<?> update(@PathVariable(value = "id") Long creditCardId, @RequestBody CreditCardJson creditCardJson) {
-        return ResponseEntity.ok(this.creditCardService.updateCreditCard(creditCardId, creditCardJson));
-    }
-
-    @ApiOperation(value = "Удаляет кредитную карту")
-    @DeleteMapping("{id}")
-    public ResponseEntity<?> delete(@PathVariable(value = "id") Long creditCardId) {
-        return ResponseEntity.ok(this.creditCardService.deleteCreditCard(creditCardId));
-    }
+//    private CreditCardService creditCardService;
+//
+//    @Qualifier("defaultCreditCardService")
+//    @Autowired(required = false)
+//    public void setCreditCardService(CreditCardService creditCardService) {
+//        this.creditCardService = creditCardService;
+//    }
+//
+//    //@ApiOperation(value = "Показвает весь список кредитных карт")
+//    @GetMapping("/all")
+//    public ResponseEntity<?> all() {
+//        return ResponseEntity.ok(this.creditCardService.showAllCreditCard());
+//    }
+//
+////    @ApiOperation(value = "Показвает кредитную карту по ID")
+//    @GetMapping("/{id}")
+//    public ResponseEntity<?> getId(@PathVariable(value = "id") Long creditCardId) {
+//        return ResponseEntity.ok(this.creditCardService.showCreditCard(creditCardId));
+//    }
+//
+////    @ApiOperation(value = "Добавляет кредитную карту")
+//    @PostMapping("/add")
+//    public ResponseEntity<?> add(
+//        @RequestParam Integer creditCardNumber,
+//        @RequestParam String holderName,
+//        @RequestParam Date expireDate) {
+//        return ResponseEntity.ok(this.creditCardService.addCreditCard(creditCardNumber, holderName, expireDate));
+//    }
+//
+////    @ApiOperation(value = "Добавляет кредитную карту посредством JSON")
+//    @PostMapping("/addJson")
+//    public ResponseEntity<?> addJson(@RequestBody CreditCardJson creditCardJson) {
+//        return ResponseEntity.ok(this.creditCardService.addCreditCardJson(creditCardJson));
+//    }
+//
+////    @ApiOperation(value = "Обновляет кредитную карту")
+//    @PatchMapping("{id}")
+//    public ResponseEntity<?> update(@PathVariable(value = "id") Long creditCardId, @RequestBody CreditCardJson creditCardJson) {
+//        return ResponseEntity.ok(this.creditCardService.updateCreditCard(creditCardId, creditCardJson));
+//    }
+//
+////    @ApiOperation(value = "Удаляет кредитную карту")
+//    @DeleteMapping("{id}")
+//    public ResponseEntity<?> delete(@PathVariable(value = "id") Long creditCardId) {
+//        return ResponseEntity.ok(this.creditCardService.deleteCreditCard(creditCardId));
+//    }
 }
