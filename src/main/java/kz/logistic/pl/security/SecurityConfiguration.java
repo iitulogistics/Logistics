e31dd2bb5bd58a3265acb5ff834b9f8cbcfc39bc
Karim <karim20.ks@gmail.com>
2019-08-02 15:32:26 +0600

    изменен SpringSecurity

--- src/main/java/kz/logistic/pl/security/SecurityConfiguration.java
+++ src/main/java/kz/logistic/pl/security/SecurityConfiguration.java
@@ -1,29 +1,49 @@
 package kz.logistic.pl.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+  @Autowired
+  DataSource repository;
 
-            //.antMatchers("/product/add", "/product/addJson").denyAll()
-            //.antMatchers("/product/add", "/product/addJson").hasAuthority("sellerCompany")
-            .antMatchers("/").permitAll()
-            .and()
-        .addFilterAfter(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeRequests()
 
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter(){
-        return new JwtAuthenticationFilter();
-    }
+      //.antMatchers("/product/add", "/product/addJson").denyAll()
+      //.antMatchers("/product/add", "/product/addJson").hasAuthority("sellerCompany")
+      .antMatchers("/").permitAll()
+      .antMatchers("/swagger-ui.html").authenticated()
+      .and()
+      .formLogin()
+      .permitAll()
+      .and()
+      .addFilterAfter(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.jdbcAuthentication()
+      .dataSource(repository)
+      .passwordEncoder(NoOpPasswordEncoder.getInstance())
+      .usersByUsernameQuery("select username, password, active from login where username=?")
+      .authoritiesByUsernameQuery("select u.username, ur.role_name from login u inner join roles ur on u.roles_id = ur.role_id where u.username=?");
+  }
+  @Bean
+  public JwtAuthenticationFilter jwtAuthenticationFilter() {
+    return new JwtAuthenticationFilter();
+  }
 
 }
